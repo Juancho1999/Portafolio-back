0177cff0e62e47e0992b0ef8de72b2acc673ae0f
Juancho <iralajua099@gmail.com>
2025-02-05 20:53:25 -0300

    corrige la ruta de inclusión de PHPMailer en sendMail.php

--- api/sendMail.php
+++ api/sendMail.php
@@ -6,10 +6,9 @@ header("Access-Control-Allow-Origin: *");
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-
-require __DIR__ . './vendor/phpmailer/phpmailer/src/PHPMailer.php';
-require __DIR__ . './vendor/phpmailer/phpmailer/src/SMTP.php';
-require __DIR__ . './vendor/phpmailer/phpmailer/src/Exception.php';
+require __DIR__ . 'phpmailer/src/PHPMailer.php';
+require __DIR__ . 'phpmailer/src/SMTP.php';
+require __DIR__ . 'phpmailer/src/Exception.php';
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
