8e982d389e7f7509987d61012df023462ee1d7d0
Juancho <iralajua099@gmail.com>
2025-02-05 21:38:01 -0300

    agrega la carga automática de Composer en sendMail.php

--- api/sendMail.php
+++ api/sendMail.php
@@ -6,6 +6,8 @@ header("Access-Control-Allow-Origin: *");
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
+require 'vendor/autoload.php';
+
 require __DIR__ . '/phpmailer/phpmailer/PHPMailer.php';
 require __DIR__ . '/phpmailer/phpmailer/SMTP.php';
 require __DIR__ . '/phpmailer/phpmailer/Exception.php';
